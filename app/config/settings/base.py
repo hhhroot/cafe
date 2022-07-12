41cdbd99888c1e868419921416c462246ca351af
hhhroot <hhhroot@safuventures.com>
2022-07-12 23:50:43 +0900

    Django initial settings.

---
+++ app/config/settings/base.py
@@ -0,0 +1,75 @@
+from pathlib import Path
+
+import environ
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+ROOT_DIR = BASE_DIR.parent
+env = environ.Env()
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+]
+
+ROOT_URLCONF = "config.urls"
+WSGI_APPLICATION = "config.wsgi.application"
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = "static/"
+STATIC_ROOT = str(ROOT_DIR / "static")
+STATICFILES_DIRS = [str(BASE_DIR / "static")]
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+MEDIA_ROOT = str(ROOT_DIR / "media")
+MEDIA_URL = "/media/"
+
+DATABASES = {"default": env.db("DATABASE_URL")}
+AUTH_USER_MODEL = "users.User"
+
+INSTALLED_APPS += [
+    # installed apps
+    "rest_framework",
+    # local_apps
+    "users.apps.UsersConfig",
+]
---
+++ app/config/settings/dev.py
@@ -0,0 +1,13 @@
+from .base import *  # noqa
+
+ALLOWED_HOSTS = ["*"]
+DEBUG = True
+
+INSTALLED_APPS += [  # noqa F405
+    "debug_toolbar",
+    "django_extensions",
+]
+
+MIDDLEWARE += [  # noqa F405
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
